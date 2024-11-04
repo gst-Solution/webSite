7e4080ef066cb596dbcaba7b8975f7f17bafc516
gst-Solution <designe.gstsolution@gmail.com>
2024-11-04 18:00:29 +0530

    hero redesign

--- components/navbar.js
+++ components/navbar.js
@@ -18,7 +18,7 @@ export default async function Navigation(){
 
     return (
         <>
-        <div className="flex z-10 text-white justify-between sticky top-0 bg-gradient-to-r from-orange-600 to-green-500">
+        <div className="flex z-20 text-white justify-between sticky top-0 bg-gradient-to-r from-orange-600 to-green-500">
             <p className="my-1 mx-10 flex"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 p-1">
   <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
 </svg>
@@ -38,7 +38,7 @@ export default async function Navigation(){
 
 
             <Navbar fluid rounded>
-      <NavbarBrand as={Link} href="https://flowbite-react.com">
+      <NavbarBrand as={Link} href="/">
         {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
         <span className="self-center text-3xl font-extrabold whitespace-nowrap bg-gradient-to-r from-orange-500 to-green-500 bg-clip-text text-transparent">Gst Solution</span>
       </NavbarBrand>
